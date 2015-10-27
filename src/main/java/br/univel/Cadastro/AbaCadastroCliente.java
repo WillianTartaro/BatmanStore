cf47b9d5f9b411f0b9a899c487f95cda52a02ed6
Willian <Willian@Willian-PC>
2015-10-27 20:37:14 -0200

    Criação da telaPrincipal e criando o "Miolo"

---
+++ src/main/java/br/univel/Cadastro/AbaCadastroCliente.java
@@ -0,0 +1,168 @@
+package br.univel.Cadastro;
+
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+
+public class AbaCadastroCliente extends JPanel {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+
+	/**
+	 * Create the panel.
+	 */
+	public AbaCadastroCliente() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblId = new JLabel("Id");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 0;
+		add(lblId, gbc_lblId);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblNome = new JLabel("Nome");
+		GridBagConstraints gbc_lblNome = new GridBagConstraints();
+		gbc_lblNome.anchor = GridBagConstraints.EAST;
+		gbc_lblNome.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNome.gridx = 0;
+		gbc_lblNome.gridy = 1;
+		add(lblNome, gbc_lblNome);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblTelefone = new JLabel("Telefone");
+		GridBagConstraints gbc_lblTelefone = new GridBagConstraints();
+		gbc_lblTelefone.anchor = GridBagConstraints.EAST;
+		gbc_lblTelefone.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTelefone.gridx = 0;
+		gbc_lblTelefone.gridy = 2;
+		add(lblTelefone, gbc_lblTelefone);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 1;
+		gbc_textField_2.gridy = 2;
+		add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblCidade = new JLabel("Endere\u00E7o");
+		GridBagConstraints gbc_lblCidade = new GridBagConstraints();
+		gbc_lblCidade.anchor = GridBagConstraints.EAST;
+		gbc_lblCidade.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCidade.gridx = 0;
+		gbc_lblCidade.gridy = 3;
+		add(lblCidade, gbc_lblCidade);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 1;
+		gbc_textField_3.gridy = 3;
+		add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblCidade_1 = new JLabel("Cidade");
+		GridBagConstraints gbc_lblCidade_1 = new GridBagConstraints();
+		gbc_lblCidade_1.anchor = GridBagConstraints.EAST;
+		gbc_lblCidade_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCidade_1.gridx = 0;
+		gbc_lblCidade_1.gridy = 4;
+		add(lblCidade_1, gbc_lblCidade_1);
+		
+		textField_4 = new JTextField();
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 1;
+		gbc_textField_4.gridy = 4;
+		add(textField_4, gbc_textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblEstado = new JLabel("Estado");
+		GridBagConstraints gbc_lblEstado = new GridBagConstraints();
+		gbc_lblEstado.anchor = GridBagConstraints.EAST;
+		gbc_lblEstado.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEstado.gridx = 0;
+		gbc_lblEstado.gridy = 5;
+		add(lblEstado, gbc_lblEstado);
+		
+		textField_5 = new JTextField();
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_5.gridx = 1;
+		gbc_textField_5.gridy = 5;
+		add(textField_5, gbc_textField_5);
+		textField_5.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email");
+		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
+		gbc_lblEmail.anchor = GridBagConstraints.EAST;
+		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEmail.gridx = 0;
+		gbc_lblEmail.gridy = 6;
+		add(lblEmail, gbc_lblEmail);
+		
+		textField_6 = new JTextField();
+		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
+		gbc_textField_6.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_6.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_6.gridx = 1;
+		gbc_textField_6.gridy = 6;
+		add(textField_6, gbc_textField_6);
+		textField_6.setColumns(10);
+		
+		JLabel lblGenero = new JLabel("Genero");
+		GridBagConstraints gbc_lblGenero = new GridBagConstraints();
+		gbc_lblGenero.anchor = GridBagConstraints.EAST;
+		gbc_lblGenero.insets = new Insets(0, 0, 0, 5);
+		gbc_lblGenero.gridx = 0;
+		gbc_lblGenero.gridy = 7;
+		add(lblGenero, gbc_lblGenero);
+		
+		textField_7 = new JTextField();
+		GridBagConstraints gbc_textField_7 = new GridBagConstraints();
+		gbc_textField_7.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_7.gridx = 1;
+		gbc_textField_7.gridy = 7;
+		add(textField_7, gbc_textField_7);
+		textField_7.setColumns(10);
+
+	}
+
+}
---
+++ src/main/java/br/univel/Cadastro/TelaPrincipal.java
@@ -0,0 +1,100 @@
+package br.univel.Cadastro;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JTabbedPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TelaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipal frame = new TelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipal() {
+		setTitle("BatmanStore");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnCadastros = new JMenu("Cadastros");
+		menuBar.add(mnCadastros);
+		
+		JMenuItem mntmCliente = new JMenuItem("Cliente");
+		mntmCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				abrirTela();
+			}
+
+			private void abrirTela() {
+				
+			}
+		});
+		mnCadastros.add(mntmCliente);
+		
+		JMenuItem mntmUsuario = new JMenuItem("Usuario");
+		mnCadastros.add(mntmUsuario);
+		
+		JMenuItem mntmProduto = new JMenuItem("Produto");
+		mnCadastros.add(mntmProduto);
+		
+		JMenu mnVendas = new JMenu("Vendas");
+		menuBar.add(mnVendas);
+		
+		JMenuItem mntmProduto_1 = new JMenuItem("Produto");
+		mnVendas.add(mntmProduto_1);
+		
+		JMenuItem mntmServios = new JMenuItem("Servi\u00E7os");
+		mnVendas.add(mntmServios);
+		
+		JMenu mnRelatorios = new JMenu("Relatorios");
+		menuBar.add(mnRelatorios);
+		
+		JMenuItem mntmClientes = new JMenuItem("Clientes");
+		mnRelatorios.add(mntmClientes);
+		
+		JMenuItem mntmProdutos = new JMenuItem("Produtos");
+		mnRelatorios.add(mntmProdutos);
+		
+		JMenuItem mntmVendas = new JMenuItem("Vendas");
+		mnRelatorios.add(mntmVendas);
+		
+		JMenuItem mntmServios_1 = new JMenuItem("Servi\u00E7os");
+		mnRelatorios.add(mntmServios_1);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+	}
+
+}
